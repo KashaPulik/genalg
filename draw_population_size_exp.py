94a8ea7b85705b1cf601637fe8dbc2f60e211c3e
Павел Кулик <is241s09@oak.ipa.csc.sibsutis.ru>
2024-05-16 22:10:16 +0700

    Провёл на кластере эксперименты, выявляющие зависимость времени от процента мутаций, количества вершин, количества итераций и размера популяции

---
+++ draw_population_size_exp.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Загрузка данных из файла с указанием разделителя столбцов
+data = pd.read_csv('data.csv', delimiter='\s+')
+
+# Извлечение данных из столбцов
+n_cities = data['n_cities']
+population_size = data['population_size']
+mutation_percent = data['mutation_percent']
+n_iterations = data['n_iterations']
+time = data['time']
+path_len = data['path_len']
+accuracy = data['accuracy']
+
+# Создание графика
+plt.figure(figsize=(10, 6))
+
+# Построение графиков
+# plt.plot(n_iterations, time, label='Time')
+# plt.plot(n_iterations, path_len, label='Path Length')
+# plt.plot(n_iterations, accuracy, label='Accuracy')
+
+plt.plot(population_size, accuracy, label='mutation_percent')
+
+# Добавление легенды и меток осей
+plt.xlabel('population_size')
+plt.ylabel('accuracy')
+plt.title('Performance Metrics vs. Number of Iterations')
+plt.legend()
+
+# Отображение графика
+plt.grid(True)
+plt.savefig('graph.png')
